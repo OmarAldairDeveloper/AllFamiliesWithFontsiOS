c8fdeb829b64b80d1fdb70d4cf47c4039613a695
Omar Aldair <omaraldair.developer@gmail.com>
2018-10-04 12:55:33 -0500

    Hemos creado un segundo viewController para mostrar en detalle las fuentes de cada familia

---
+++ AllFamiliesWithFonts/DetailFontsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailFontsViewController.swift
+//  AllFamiliesWithFonts
+//
+//  Created by Omar Aldair Romero Pérez on 10/4/18.
+//  Copyright © 2018 Omar Aldair Romero Pérez. All rights reserved.
+//
+
+import UIKit
+
+class DetailFontsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
